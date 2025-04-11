2b99a541d63724033bd2617dc4e89f4067153d41
Javier Soria <Poempolo7@gmail.com>
2025-04-11 11:15:30 +0200

    v0.1.1: Added password generation logic and html structure.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
 "use strict";
+// npx tsc al guardar
+function generatePassword(length, useUpper, useLower, useNumbers, useSymbols) {
+    const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowers = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    let characters = '';
+    if (useUpper)
+        characters += uppers;
+    if (useLower)
+        characters += lowers;
+    if (useNumbers)
+        characters += numbers;
+    if (useSymbols)
+        characters += symbols;
+    if (!characters)
+        return '';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randIndex];
+    }
+    return password;
+}
---
+++ src/script.ts
@@ -0,0 +1,25 @@
+// npx tsc al guardar
+
+function generatePassword(length: number, useUpper: boolean, useLower: boolean, 
+    useNumbers: boolean, useSymbols: boolean): string {
+        const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const lowers = 'abcdefghijklmnopqrstuvwxyz';
+        const numbers = '0123456789';
+        const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+        let characters = '';
+        if (useUpper) characters += uppers;
+        if (useLower) characters += lowers;
+        if (useNumbers) characters += numbers;
+        if (useSymbols) characters += symbols;
+
+        if(!characters) return '';
+
+        let password = '';
+        for (let i = 0; i < length; i++) {
+            const randIndex = Math.floor(Math.random() * characters.length);
+            password += characters[randIndex];
+        }
+
+        return password;
+}
