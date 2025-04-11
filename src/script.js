cb07b7fe79fb00be9e1aa0d95743b7b0b3f75268
Javier Soria <Poempolo7@gmail.com>
2025-04-11 11:40:06 +0200

    v0.2: Functional prototype

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 "use strict";
 // npx tsc al guardar
+const lengthInput = document.getElementById('length');
+const uppercaseCheckbox = document.getElementById('uppercase');
+const lowercaseCheckbox = document.getElementById('lowercase');
+const numbersCheckbox = document.getElementById('numbers');
+const symbolsCheckbox = document.getElementById('symbols');
+const passwordOutput = document.getElementById('password');
+const generateButton = document.getElementById('generate');
+const copyButton = document.getElementById('copy');
 function generatePassword(length, useUpper, useLower, useNumbers, useSymbols) {
     const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const lowers = 'abcdefghijklmnopqrstuvwxyz';
@@ -23,3 +31,16 @@ function generatePassword(length, useUpper, useLower, useNumbers, useSymbols) {
     }
     return password;
 }
+generateButton.addEventListener('click', () => {
+    const lenght = parseInt(lengthInput.value);
+    const useUpper = uppercaseCheckbox.checked;
+    const useLower = lowercaseCheckbox.checked;
+    const useNumbers = numbersCheckbox.checked;
+    const useSymbols = symbolsCheckbox.checked;
+    const password = generatePassword(lenght, useUpper, useLower, useNumbers, useSymbols);
+    passwordOutput.value = password;
+});
+copyButton.addEventListener('click', () => {
+    passwordOutput.select();
+    document.execCommand('copy');
+});
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,14 @@
 // npx tsc al guardar
 
+const lengthInput = document.getElementById('length') as HTMLInputElement;
+const uppercaseCheckbox = document.getElementById('uppercase') as HTMLInputElement;
+const lowercaseCheckbox = document.getElementById('lowercase') as HTMLInputElement;
+const numbersCheckbox = document.getElementById('numbers') as HTMLInputElement;
+const symbolsCheckbox = document.getElementById('symbols') as HTMLInputElement;
+const passwordOutput = document.getElementById('password') as HTMLInputElement;
+const generateButton = document.getElementById('generate') as HTMLButtonElement;
+const copyButton = document.getElementById('copy') as HTMLButtonElement;
+
 function generatePassword(length: number, useUpper: boolean, useLower: boolean, 
     useNumbers: boolean, useSymbols: boolean): string {
         const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -23,3 +32,19 @@ function generatePassword(length: number, useUpper: boolean, useLower: boolean,
 
         return password;
 }
+
+generateButton.addEventListener('click', () =>{
+    const lenght = parseInt(lengthInput.value);
+    const useUpper = uppercaseCheckbox.checked;
+    const useLower = lowercaseCheckbox.checked;
+    const useNumbers = numbersCheckbox.checked;
+    const useSymbols = symbolsCheckbox.checked;
+
+    const password = generatePassword(lenght, useUpper, useLower, useNumbers, useSymbols);
+    passwordOutput.value = password;
+});
+
+copyButton.addEventListener('click', () =>{
+    passwordOutput.select();
+    document.execCommand('copy');
+});
